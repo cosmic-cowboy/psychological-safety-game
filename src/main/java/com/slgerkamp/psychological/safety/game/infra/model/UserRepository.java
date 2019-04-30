634937cbea8e794cfbdc7386f101b6400d6e224a
hiroo takaku <slgerkamp@gmail.com>
2019-04-30 20:38:07 +0900

    delete user info

--- src/main/java/com/slgerkamp/psychological/safety/game/infra/model/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.slgerkamp.psychological.safety.game.infra.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
--- src/main/resources/db/migration/V005__create-users-table.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE users (
-  id           VARCHAR PRIMARY KEY,
-  status       VARCHAR NOT NULL
-);
