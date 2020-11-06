eaf99389fc2cabf281736348163aeb2c040cd12a
hiroo-takaku <hiroo.takaku@linecorp.com>
2020-11-06 21:45:33 +0900

    update nav section

--- src/main/java/com/slgerkamp/psychological/safety/game/application/config/SecurityConfig.java
+++ src/main/java/com/slgerkamp/psychological/safety/game/application/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.slgerkamp.psychological.safety.game.application.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -22,4 +23,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 "/favicon.ico");
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/index").permitAll()
+                .anyRequest().authenticated();
+    }
 }
